78ab6b540343456ed62cc275e383896ad549157b
TuftedDeer <36223345+tufteddeer@users.noreply.github.com>
2023-12-17 11:36:31 +0100

    feat(barmband)!: log to mqtt
    Needs config file changes to avoid duplicate implementations, see example

---
+++ src/logging.cpp
@@ -0,0 +1,41 @@
+#include "logging.h"
+#include "config.h"
+
+namespace barmband::log {
+
+AsyncMqttClient* loggingMqttClient;
+
+void setLoggingMqttclient(AsyncMqttClient* client) {
+  loggingMqttClient = client;
+}
+
+void logln(String id, const char* msg) {
+  Serial.println(msg);
+
+  #ifdef MQTT_LOGGING_TOPIC
+  if (loggingMqttClient != nullptr) {
+    char msgBuff[80];
+    sprintf(msgBuff, "%s: %s", id.c_str(), msg);
+    loggingMqttClient->publish(MQTT_LOGGING_TOPIC, 2, true, msgBuff);
+  }
+  #endif
+}
+
+void logf(String id, const char* fmt, ...) {
+  char msgBuff[80];
+
+  va_list va;
+  va_start(va, fmt);
+  vsprintf(msgBuff, fmt, va);
+  Serial.print(msgBuff);
+  va_end(va);
+
+  #ifdef MQTT_LOGGING_TOPIC
+  if (loggingMqttClient != nullptr) {
+    char msgWithId[91];
+    sprintf(msgWithId, "%s: %s", id.c_str(), msgBuff);
+    loggingMqttClient->publish(MQTT_LOGGING_TOPIC, 2, true, msgWithId);
+  }
+  #endif
+}
+}  // namespace barmband::log
